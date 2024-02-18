b0ba5c54a2306091b70c833c25d2f37d9c17d70f
kylekim95 <kylekim95@gmail.com>
2024-02-18 10:18:08 +0900

    02/18
    gitignore Saved Intermediate Binaries fix attempt 1

--- Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Slate/SharedPCH.Slate.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.37.32825
-#include "D:/Dev/UnrealProjects/Pond/Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Slate/SharedPCH.Slate.Cpp20.h"
--- Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "D:/Dev/UnrealProjects/Pond/Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
