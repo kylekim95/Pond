c8269ca07c2856883da0e79de10cdd6ad5d1fc17
kylekim95 <kylekim95@gmail.com>
2024-03-31 15:01:34 +0900

    Revert "asdf"
    This reverts commit 85401f85d0c8ebebc90662b254bb924680f048d2.

--- Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "D:/Dev/UnrealProjects/Pond/Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Dev/UnrealProjects/Pond/Pond/Intermediate/Build/Win64/x64/PondEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
