b73a75b256fa91e0a2e195f7a70913186b34cdb7
kylekim95 <kylekim95@gmail.com>
2024-04-28 13:56:53 +0900

    4/28
    Crosshair

--- Pond/Source/Pond/Private/CrosshairWidget.cpp
+++ Pond/Source/Pond/Private/CrosshairWidget.cpp
@@ -2,4 +2,35 @@
 
 
 #include "CrosshairWidget.h"
+#include "Components/Image.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
 
+UCrosshairWidget::UCrosshairWidget(const FObjectInitializer& ObjectInitializer)
+    :UUserWidget(ObjectInitializer)
+{
+    static ConstructorHelpers::FObjectFinder<UTexture2D> DefaultCursorImgLoader(TEXT("/Script/Engine.Texture2D'/Game/UserContent/Crosshair.Crosshair'"));
+    if(DefaultCursorImgLoader.Succeeded()){
+        DefaultCursorImg = DefaultCursorImgLoader.Object;
+    }
+}
+
+void UCrosshairWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+    CustomCursor->SetBrushFromTexture(DefaultCursorImg);
+}
+
+void UCrosshairWidget::MoveCursor(FVector2D Input)
+{
+    UCanvasPanelSlot* CanvasPanelSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(CursorWrapper);
+    if(IsValid(CanvasPanelSlot)){
+        // FVector2D CurrentPosition = CanvasPanelSlot->GetPosition();
+        // Input.Normalize();
+        // Input.Y *= -1;
+        CanvasPanelSlot->SetPosition(Input);
+
+        UE_LOG(LogTemp, Warning, TEXT("%f, %f"), Input.X, Input.Y);
+    }
+}
